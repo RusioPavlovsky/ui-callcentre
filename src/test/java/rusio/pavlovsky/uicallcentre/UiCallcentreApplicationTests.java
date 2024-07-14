490664701e290a52e4269ff546d4093225830402
rusla <rpavlov@ubic.tech>
2024-07-14 03:35:26 +0300

    initial commit

---
+++ src/test/java/rusio/pavlovsky/uicallcentre/UiCallcentreApplicationTests.java
@@ -0,0 +1,13 @@
+package rusio.pavlovsky.uicallcentre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiCallcentreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
